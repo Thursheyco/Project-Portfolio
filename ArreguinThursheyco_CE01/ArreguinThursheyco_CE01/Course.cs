c699906ee2b20c1d1ec86e462494a58e69f97dae
Thursheyco <sheycoa@gmail.com>
2019-07-10 14:28:21 -0400

    FIX--renamed Item class to Course class

---
+++ ArreguinThursheyco_CE01/ArreguinThursheyco_CE01/Course.cs
@@ -0,0 +1,20 @@
+﻿// Thursheyco Arreguin
+// 07-08-19
+// DVP3 1907
+// Code Exercise 01 - Event Handler
+
+namespace ArreguinThursheyco_CE01
+{
+    // will hold data entered by the user on the UserInput form
+    public class Course
+    {
+        public string Title { get; set; }
+        public bool Done { get; set; }
+
+        // override ToString method to only return Title of Item
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
